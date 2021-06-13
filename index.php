c035220462072f5be44a5ef724f87e8012d5055e
Afonso Ngindu <afonsonkidisala@hotmail.com>
2021-06-13 04:18:07 +0300

    Meu portfolio

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Afonso Nkidisala</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<header>
+		<div class="center">
+			<div class="logo">
+				
+				<h1>Nkidisala</h1>
+			</div>
+			<div class="menu-desktop">
+				<ul>
+					<li>
+						<a href="#">Menu</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+					<li>
+						<a href="#">Skills</a>
+					</li>
+					
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		
+		</div>
+		<div class="clear"></div>
+	</header>
+	<section class="menu-mobile">
+		<div class="center">
+			<div class="mobile">
+				<ul>
+					<li>
+						<a href="#">Menu</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+					<li>
+						<a href="#">Skills</a>
+					</li>
+					
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</section>
+	<section class="cta">
+		<div class="center">
+			<div class="cta-text">
+				<h1>Hi,i'm Afonso Nkidisala</h1>
+				<span> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.
+			consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua</span>
+				<button>Contact</button>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</section>
+	<section class="about">
+		<div class="center">
+			<div class="tex-about">
+			<h1>About Me</h1>
+			<span></span>
+			</div>
+			<div class="container-about">
+			<div class="im-about">
+				<img src="imagens/n.jpg">
+			</div>
+			<div class="about-text">
+				<span> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+			</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</section>
+	<section class="skills">
+		<div class="center">
+			<div class="skills-tex">
+				<h1>My skills</h1>
+				<span></span>
+			</div>
+			<div class="skills-all">
+			<div class="box-skill">
+				<img src="imagens/h.png">
+			</div>
+			<div class="box-skill">
+
+				<img src="imagens/cs.png">
+				<span></span>
+			</div>
+		
+		
+			<div class="box-skill">
+				<img src="imagens/j.webp">
+			</div>
+			<div class="box-skill">
+				<img src="imagens/jq3.png">
+			</div>
+			<div class="box-skill">
+				<img src="imagens/p.png">
+			</div>
+			
+		</div>
+		</div>
+		<div class="clear"></div>
+	</section>
+	<section class="services">
+		<div class="center">
+			<div class="services-tex">
+				<h1>Portfolios</h1>
+				<span></span>
+			</div>
+			<div class="services-all">
+			<div class="box-services">
+				<img src="imagens/ef.png">
+			</div>
+			<div class="box-services">
+				<img src="imagens/ef2.png">
+			</div>
+			<div class="box-services">
+				<img src="imagens/ef.png">
+			</div>
+			
+		</div>
+		</div>
+		<div class="clear"></div>
+	</section>
+	<section class="form">
+		<div class="center">
+			<form>
+				<input type="name" placeholder="Digite seu nome" name="nome">
+				<input type="email" placeholder="Digite seu email" name="senha">
+				<input type="text" placeholder="Deixe a sua mensagens" name="sms">
+				<input type="submit" name="enviar">
+			</form>
+		</div>
+		<div class="clear"></div>
+	</section>
+	<footer>
+		<div class="clear">
+			<div class=""></div>
+			<div class="">
+				<ul>
+					<li>
+						<a href=""><i class="fa fa-facebook"></i></a>
+					</li>
+					<li>
+						<a href=""><i class="fa fa-instagram"></i></a>
+					</li>
+					<li>
+						<a href=""><i class="fa fa-github"></i></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
